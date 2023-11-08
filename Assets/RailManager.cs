8c07998273056abcc75c3806224dec93b2607d09
Kenneth Massao Straumsheim <kmstraumsheim@gmail.com>
2023-11-08 16:58:40 +0100

    Initial commit

---
+++ Assets/RailManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailManager : MonoBehaviour
+{
+    [SerializeField] List<RailNode> railNodes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
